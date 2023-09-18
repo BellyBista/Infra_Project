755b28fa781a6bee98465eed276a0e1ac9d38423
quadribello <quadribello41@gmail.com>
2023-09-18 15:35:36 -0400

    Contributing-to: https://kennedymakhanu1.atlassian.net/browse/CLD-3

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type    = string
+  default = "Quadri"
+}
+
+variable "versioning" {
+  type    = string
+  default = "Enabled"
+}
+
+variable "create_vpc" {
+  type    = bool
+  default = true
+}
