108292bb6db3e3b9f01b82289e744d652d99302a
quadribello <quadribello41@gmail.com>
2023-09-19 13:58:10 -0400

    chore: update scripts to module
    Contributing-to: https://kennedymakhanu1.atlassian.net/browse/CLD-03

---
+++ s3-module/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "versioning" {
+  type    = string
+  default = "Enabled"
+}
+
+variable "create_vpc" {
+  type    = bool
+  default = true
+}
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
