16783dd08bdf5ef67fdcb069d3256794fe3670df
quadribello <quadribello41@gmail.com>
2023-09-20 16:08:35 -0400

    feat: remove provider block

--- module-call/main.tf
+++ module-call/main.tf
@@ -1,3 +1,5 @@
 module "s3" {
-  source = "git::https://github.com/quadribello/Infra_Project.git//s3-module"
+  source = "git::https://github.com/quadribello/Infra_Project.git//s3-module?ref=v1.0.2"
+  region = "us-east-1"
+  env = "development"
 }
