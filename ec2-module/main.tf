c2ea54bc91cccbc52e50e0b29f830b8b90f4e70d
quadribello <quadribello41@gmail.com>
2023-09-22 22:27:01 -0400

    fix: remove vpc_id variable

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.amzlinux2.id
   instance_type = var.my_instance_type
-  vpc_id        = data.terraform_remote_state.vpc.outputs.vpc_id
 
   tags = {
     "Name" = var.name
