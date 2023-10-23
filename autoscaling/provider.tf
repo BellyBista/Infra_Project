922a0b0cfa2d2cba11613a59aa7dfbeb7c10ae9c
quadribello <quadribello41@gmail.com>
2023-10-23 14:36:35 -0400

    feat: added autosccaling to eks

---
+++ autoscaling/provider.tf
@@ -0,0 +1,21 @@
+# Terraform block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider block
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "network" {
+  backend = "local"
+  config = {
+    path = "../test-oidc/terraform.tfstate"
+  }
+}
---
+++ test-oidc/thumbprint.py
@@ -0,0 +1,5 @@
+input_string = ""
+output_string = input_string.replace(":", "")
+
+print("Original String:", input_string)
+print("Modified String:", output_string)
